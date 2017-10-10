f46f690e2830dfcd00ca13633bccabd6518453fe
redfindiver <redfindiver@gmail.com>
2017-10-10 22:08:41 +0200

    improved the calculation of  years to show in the filter

--- component/admin/models/fields/trainyears.php
+++ component/admin/models/fields/trainyears.php
@@ -13,68 +13,62 @@ defined('_JEXEC') or die;
  */
 class JFormFieldTrainyears extends JFormFieldList
 {
-        /**
-         * The form field type.
-         *
-         * @var		string
-         * @since   2.0
-         */
-        protected $type = 'trainyears';
+    /**
+     * The form field type.
+     *
+     * @var		string
+     * @since   2.0
+     */
+    protected $type = 'trainyears';
+    
+    /**
+     * Method to get the field input markup.
+     *
+     * @return  string	The field input markup.
+     *
+     * @since   2.0
+     */
+    public function getOptions()
+    {   
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true);
+
+        $query->select('DISTINCT substring(date,1,4)');
+        $query->from($db->quoteName('#__tkdclub_trainings'));
+        $query->order($db->quoteName('date') . 'ASC');
+
+        $db->setQuery($query);
         
-        /**
-         * Method to get the field input markup.
-         *
-         * @return  string	The field input markup.
-         *
-         * @since   2.0
-         */
-        public function getOptions()
-        {   
-            $db = JFactory::getDbo();
-            $query = $db->getQuery(true);
+        if ($years = $db->loadColumn())
+        {
+            //maximum years to show as set in the configuration
+            $number_years = JComponentHelper::getParams('com_tkdclub')->get('training_years', 5);
+            $min_year = max($years) - $number_years;
 
-            $query->select('DISTINCT substring(date,1,4)');
-            $query->from($db->quoteName('#__tkdclub_trainings'));
-            $query->order($db->quoteName('date') . 'ASC');
-
-            $db->setQuery($query);
-            
-            if ($years = $db->loadColumn())
+            foreach ($years as $key => $year)
             {
-                //maximum years to show, calculation if years should be deleted from array
-                $number_years = JComponentHelper::getParams('com_tkdclub')->get('training_years', 5);
-                $years_in_array = count($years)-1;
-                
-                if (($years_in_array - $number_years) > 0)
-                {
-                    $years_to_delete = ($number_years - $years_in_array) * -1;
-                    
-                    
-                    for ($i = 0; $years_to_delete > 0; $i++)
-                    {
-                        unset($years[$i]);
-                        $years_to_delete--;
-                    }
-                }
-                
-                foreach($years as $year)
-                {
-
-                    $options[] = JHtml::_('select.option', $year, $year);
-
-                }
-                
-                $options = array_reverse($options);
-
-                if ($this->form) //checking if we are in a form, then merge additional xml data
-                {
-                    $options = array_merge(parent::getOptions(), $options); 
-                }
-                
-                return $options;
+               if ($year < $min_year)
+               {
+                   unset($years[$key]);
+               }
             }
             
-            return parent::getOptions();
+            foreach($years as $year)
+            {
+                $options[] = JHtml::_('select.option', $year, $year);
+            }
+            
+            $options = array_reverse($options);
+
+            if ($this->form) //checking if we are in a form, then merge additional xml data
+            {
+                $options = array_merge(parent::getOptions(), $options); 
+            }
+            
+            return $options;
         }
-    
+        
+        return parent::getOptions();
+    }
+
 }
