adefcfa251e48a137db416bf58d309f9524caa8a
redfindiver <redfindiver@gmail.com>
2017-10-11 23:04:28 +0200

    controller secured and some input improvements

--- component/admin/controller.php
+++ component/admin/controller.php
@@ -14,11 +14,32 @@ class TkdClubController extends JControllerLegacy
     protected $default_view = 'members';
 
     public function display($cachable = false, $urlparams = false)
-	{
-		$input  = JFactory::getApplication()->input;        
-		$view   = $input->get('view', $this->default_view);
-		$layout = $input->get('layout', 'default');     
-        TkdclubHelperSidebar::addSubmenu($view);
+	{    
+		$view   = $this->input->get('view', $this->default_view);
+		$layout = $this->input->get('layout', 'default');
+		$id     = $this->input->getInt($view . '_id');     
+		TkdclubHelperSidebar::addSubmenu($view);
+		
+		// Check for edit form.
+		if ($view == 'member' && $layout == 'edit' && !$this->checkEditId('com_tkdclub.edit.member', $id))
+		{
+			// Somehow the person just went to the form - we don't allow that.
+			$this->setError(JText::sprintf('JLIB_APPLICATION_ERROR_UNHELD_ID', $id));
+			$this->setMessage($this->getError(), 'error');
+			$this->setRedirect(JRoute::_('index.php?option=com_tkdclub&view=members', false));
+
+			return false;
+		}
+		
+		if ($view == 'training' && $layout == 'edit' && !$this->checkEditId('com_tkdclub.edit.training', $id))
+		{
+			// Somehow the person just went to the form - we don't allow that.
+			$this->setError(JText::sprintf('JLIB_APPLICATION_ERROR_UNHELD_ID', $id));
+			$this->setMessage($this->getError(), 'error');
+			$this->setRedirect(JRoute::_('index.php?option=com_tkdclub&view=trainings', false));
+
+			return false;
+		}
 
         parent::display($cachable, $urlparams);
 	}
