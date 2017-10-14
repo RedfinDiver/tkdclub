dff958928c9d432108c8bbbf0cda00f52b845c81
redfindiver <redfindiver@gmail.com>
2017-10-14 13:56:19 +0200

    improved helpers method

--- component/admin/helpers/members.php
+++ component/admin/helpers/members.php
@@ -7,50 +7,41 @@
 defined('_JEXEC') or die;
 
 /**
- * Helper-class for com_tkdclub
  * With this class it is possible to get members names according to ids
  * Used in:
  * /views/medals/view.html.php
  * /views/medals/default.php
  * /controllers/export.raw.php
- *
- * @since  2.0
  */
 class TkdclubHelperMembers
 {       
     /**
-    * Method get the name of a member by id
-    *
-    * @return  mixed    FALSE if there is no data
-    *                   ARRAY $attachments with data otherwise
-    *
-    * @since   2.1
-    */
+     * Method get the name of a member by id
+     *
+     * @return  mixed    FALSE if there is no data
+     *                   STRING string with names, diveded with " / "
+     */
     public function getMembersNames($ids, $memberlist)
     {   
-        $names = '';
-        if (is_numeric($ids)) // just one winner -> one integer value
-        {
-            $names =  $memberlist[$ids];
-        }
-        else
-        {   
-            $name_ids = explode(',', $ids);
-            $i = count((array) $name_ids);
-            $it = 0;
-            foreach ( $name_ids as $name_id)
-            {
-                if ($i == 1) { $names .=  $memberlist[$name_id]; } // only one winner
-                else 
-                {   
-                    $it += 1;
-                    $names .= $memberlist[$name_id];
-                    if ($it < $i) {$names .= ' / ';}
-                }
-            }
-        }    
+        $ids = json_decode($ids);
+        $i = count($ids);
 
-        return $names;
+        if ($i == 0)
+        {
+            return false;
+        }
+
+        $names = '';
+        $it = 0;
+
+        foreach ($ids as $id)
+        {
+            $it += 1;
+            $names .= $memberlist[$id];
+            if ($it < $i) {$names .= ' / ';}
+        }
+
+        return $names; 
     }
 
     /**
@@ -60,25 +51,23 @@ class TkdclubHelperMembers
      *
      */
     public function getMemberlist()
+    {
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true);
+        $fields = array('member_id', 'firstname', 'lastname');
+        
+        $query->select($db->quoteName($fields))
+              ->from($db->quoteName('#__tkdclub_members'));
+        $db->setQuery($query);
+        $items = $db->loadObjectList();
+
+        // create arrays, $key = member_id, $value = 'firstname lastname'
+        $memberlist = array();
+        foreach ($items as $i => $item)
         {
-             $db = JFactory::getDbo();
-             $query = $db->getQuery(true);
+            $memberlist[$item->member_id] = $item->firstname.' '.$item->lastname;
+        }
 
-             // selecting the members
-             $fields = array('member_id', 'firstname', 'lastname');
-             $query->select($db->quoteName($fields))
-                   ->from($db->quoteName('#__tkdclub_members'));
-
-                 $db->setQuery($query);
-             $items = $db->loadObjectList();
-
-             // create arrays, $key = member_id, $value = 'firstname lastname'
-             $namelist = array();
-             foreach ($items as $i => $item)
-             {
-                 $namelist[$item->member_id] = $item->firstname.' '.$item->lastname;
-             }
-
-             return $namelist;
-         }
+        return $memberlist;
+    }
 }
