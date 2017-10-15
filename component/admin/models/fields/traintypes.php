de3cfe2eece8c7bb38033ab973b660a5a53950fa
redfindiver <redfindiver@gmail.com>
2017-10-15 16:01:32 +0200

    improved traintypes and trainer fields

--- component/admin/models/fields/traintypes.php
+++ component/admin/models/fields/traintypes.php
@@ -23,22 +23,51 @@ class JFormFieldTraintypes extends JFormFieldList
      * Method to get the field input markup for field 'traintypes'
      */    
     public function getOptions()
-    {
-        $types = TkdClubHelperList::getList(JComponentHelper::getParams('com_tkdclub')->get('training_types'));
+    {   
+        $options = array();
 
-        if ($types)
+        // check if field is used as filter, then get options from the database entries
+        if ($this->element['isFilter'] == 'true')
         {
-            foreach($types as $type)
+            $db = JFactory::getDbo();
+            $query = $db->getQuery(true);
+            $query->select('DISTINCT(type)');
+            $query->from($db->quoteName('#__tkdclub_trainings'));
+            $query->order('type ASC');
+            $db->setQuery($query);
+
+            if ($types = $db->loadColumn())
             {
-                $options[] = JHtml::_('select.option', $type, $type);
+                foreach($types as $type)
+                {
+                    $options[] = JHtml::_('select.option', $type, $type);
+                }
             }
+            
+        }
+        else // not used as filter field, so get the training types from the parameter
+        {
+            $types = TkdClubHelperList::getList(JComponentHelper::getParams('com_tkdclub')->get('training_types'));
 
-            $options = array_merge(parent::getOptions(), $options);
-
-            return $options; 
+            if (!$types)
+            {
+                JFactory::getApplication()->enqueueMessage(JText::_('COM_TKDCLUB_TRAINING_NO_TRAINTYPES_DEFINED'), 'warning');
+            }
+            else
+            {
+                foreach($types as $type)
+                {
+                    $options[] = JHtml::_('select.option', $type, $type);
+                }
+            }
         }
 
-        return parent::getOptions();
+        if ($this->form) // checking if we are in a form, then merge additional xml data
+        {
+            $options = array_merge(parent::getOptions(), $options); 
+        }
+
+        return $options;
     }
     
 }
