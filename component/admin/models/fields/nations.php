48f2ac0cb4cf0704bc8fbf98ce6230c6d71148f1
RedfinDiver <redfindiver@gmail.com>
2017-11-17 18:40:33 +0100

    deleted some unnecessary imports

--- component/admin/models/fields/nations.php
+++ component/admin/models/fields/nations.php
@@ -1,16 +1,12 @@
 <?php
 /**
- * @package     Administrator
- * @subpackage	com_tkdclub
- * @copyright	Copyright (C) 2016 Markus Moser, Inc. All rights reserved.
- * @license	GNU General Public License version 2 or later
- */
+* @package    Taekwondo Club
+* @copyright  Copyright (C) 2017 Markus Moser. All rights reserved.
+* @license    GNU General Public License version 2 or later; see LICENSE.txt
+*/
 
 defined('_JEXEC') or die;
 
-jimport('joomla.form.formfield');
-jimport('joomla.form.helper');
-JFormHelper::loadFieldClass('list');
 JLoader::register('TkdClubHelperList', JPATH_COMPONENT_ADMINISTRATOR. '/helpers/list.php');
 
 /**
@@ -20,34 +16,34 @@ JLoader::register('TkdClubHelperList', JPATH_COMPONENT_ADMINISTRATOR. '/helpers/
  */
 class JFormFieldNations extends JFormFieldList
 {       
-        /**
-         * The form field type.
-         *
-         * @var		string
-         * @since   1.0
-         */
-        protected $type = 'nations';
+    /**
+     * The form field type.
+     *
+     * @var		string
+     * @since   1.0
+     */
+    protected $type = 'nations';
 
-       /**
-         * Method to get the field input markup.
-         *
-         * @return  string	The field input markup.
-         *
-         * @since   1.0
-         */        
-        public function getOptions()
+    /**
+     * Method to get the field input markup.
+     *
+     * @return  string	The field input markup.
+     *
+     * @since   1.0
+     */        
+    public function getOptions()
+    {
+        $nations = TkdClubHelperList::getList(JComponentHelper::getParams('com_tkdclub')
+                    ->get('nations', ',AUT,GER,SUI,ITA,CRO,BIH,SRB'));
+
+        foreach($nations as $nation)
         {
-            $nations = TkdClubHelperList::getList(JComponentHelper::getParams('com_tkdclub')
-                       ->get('nations', ',AUT,GER,SUI,ITA,CRO,BIH,SRB'));
-
-            foreach($nations as $nation)
-            {
-                $options[] = JHtml::_('select.option', $nation, $nation);
-            }
-
-            $options = array_merge(parent::getOptions(), $options);
-            
-            return $options;  
+            $options[] = JHtml::_('select.option', $nation, $nation);
         }
+
+        $options = array_merge(parent::getOptions(), $options);
+        
+        return $options;  
+    }
         
 }
