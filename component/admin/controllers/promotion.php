8afe3a2f69fd9ab067f7b7d068d200f58ca5a9c6
RedfinDiver <redfindiver@gmail.com>
2017-12-08 23:48:21 +0100

    improved metadata and ison conversion

--- component/admin/controllers/promotion.php
+++ component/admin/controllers/promotion.php
@@ -10,36 +10,4 @@ defined('_JEXEC') or die;
 class TkdClubControllerPromotion extends JControllerForm
 {
     protected $text_prefix = 'COM_TKDCLUB_PROMOTION';
-
-    /**
-     * Overriding save method for additional info about
-     * modifiy and create date
-    */
-    public function save($key = null, $urlVar = null)
-    {
-        //getting the saving user and the current datetime
-        $user_id = JFactory::getUser()->id;
-        $date = JFactory::getDate()->toSql();
-        //getting the data array
-        $fields = $this->input->post->get('jform', array(), 'array');
-
-        if ($this->input->get('promotion_id') > 0)
-        {                    
-            //already existing item, just changing the modified fields
-            $fields['modified'] = $date;
-            $fields['modified_by'] = intval($user_id);
-        }
-        elseif ($this->input->get('promotion_id') == 0)
-        {
-            //new created item, just setting create fields
-            $fields['created'] = $date;
-            $fields['created_by'] = intval($user_id);            
-        }
-        
-        //pushing back the fields to the input object
-        $this->input->post->set('jform', $fields);
-        
-        parent::save($key, $urlVar);
-    }
-    
 }
--- component/admin/tables/promotions.php
+++ component/admin/tables/promotions.php
@@ -17,4 +17,25 @@ class TkdClubTablePromotions extends JTable
         $this->setColumnAlias('published', 'promotion_state'); // needed for autoworking of publish-method
         parent::__construct('#__tkdclub_promotions', 'promotion_id', $db);
     }
+
+    public function store($updateNulls = false) {
+        
+        $date   = JFactory::getDate()->toSql();
+        $userId = JFactory::getUser()->id;
+
+        $this->modified = $date;
+
+        if ($this->promotion_id)
+        {
+            // Existing item
+            $this->modified_by = $userId;
+        }
+        else
+        {
+            $this->created = $date;
+            $this->created_by = $userId;
+        }
+        
+        return parent::store($updateNulls = false);
+    }
 }
