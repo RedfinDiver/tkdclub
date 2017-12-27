ff1993e5b6fef5e7aac4156a60c92c644e6741f6
RedfinDiver <redfindiver@gmail.com>
2017-12-27 13:45:36 +0100

    code improvements

--- component/admin/models/medal.php
+++ component/admin/models/medal.php
@@ -14,24 +14,24 @@ defined('_JEXEC') or die;
  */
 class TkdClubModelMedal extends JModelAdmin
 {
-        /**
-         * Method to get a table object, load it if necessary.
-         *
-         * @param   string  $type     The table name. Optional.
-         * @param   string  $prefix   The class prefix. Optional.
-         * @param   array   $config  Configuration array for model. Optional.
-         *
-         * @return  JTable  A JTable object
-         *
-         * @since   1.0
-         * @throws  Exception
-         */
-        public function getTable($type = 'Medals', $prefix = 'TkdClubTable', $config = array())
-        {
-            return JTable::getInstance($type, $prefix, $config);
-        }
+    /**
+     * Method to get a table object, load it if necessary.
+     *
+     * @param   string  $type     The table name. Optional.
+     * @param   string  $prefix   The class prefix. Optional.
+     * @param   array   $config   Configuration array for model. Optional.
+     *
+     * @return  JTable  A JTable object
+     *
+     * @since   1.0
+     * @throws  Exception
+     */
+    public function getTable($type = 'Medals', $prefix = 'TkdClubTable', $config = array())
+    {
+        return JTable::getInstance($type, $prefix, $config);
+    }
         
-        /**
+    /**
 	 * Method for getting the form from the model.
 	 *
 	 * @param   array    $data      Data for the form.
@@ -41,56 +41,56 @@ class TkdClubModelMedal extends JModelAdmin
 	 *
 	 * @since   1.0
 	 */
-        public function getForm($data = array(), $loadData = true)
-        {   
-            $options = array('control' => 'jform', 'load_data' => $loadData);
-            $form = $this->loadForm('tkdclub', 'medal',  $options);
+    public function getForm($data = array(), $loadData = true)
+    {   
+        $options = array('control' => 'jform', 'load_data' => $loadData);
+        $form = $this->loadForm('tkdclub', 'medal',  $options);
 
-            if (empty($form))
-            {
-                return false;
-            }
-            
-            return $form;
+        if (empty($form))
+        {
+            return false;
         }
+        
+        return $form;
+    }
 
-        /**
+    /**
 	 * Method to get the data that should be injected in the form.
 	 *
 	 * @return  array    The default data is an empty array.
 	 *
 	 * @since   1.0
 	 */
-        protected function loadFormData()
+    protected function loadFormData()
+    {
+        $app =  JFactory::getApplication();
+        $data = $app->getUserState('com_tkdclub.edit.medal.data', array());
+
+        if(empty($data))
         {
-            $app =  JFactory::getApplication();
-            $data = $app->getUserState('com_tkdclub.edit.medal.data', array());
-
-            if(empty($data))
-            {
-                $data = $this->getItem();
-            }
-            
-            return $data;
+            $data = $this->getItem();
         }
+        
+        return $data;
+    }
 
-        /**
-        * Method to get a single record.
-        *
-        * @param   integer  $pk  The id of the primary key.
-        *
-        * @return  JObject|boolean  Object on success, false on failure.
-        *
-        * @since   12.2
-        */
-        public function getItem($pk = null)
-        {   
-            // Handling the String for multible field
-            $items = parent::getItem($pk);
-            $ids = json_decode($items->get('winner_ids'));
-            $items->set('winner_ids',$ids);
+    /**
+    * Method to get a single record.
+    *
+    * @param   integer  $pk  The id of the primary key.
+    *
+    * @return  JObject|boolean  Object on success, false on failure.
+    *
+    * @since   12.2
+    */
+    public function getItem($pk = null)
+    {   
+        // Handling the String for multible field
+        $items = parent::getItem($pk);
+        $ids = json_decode($items->get('winner_ids'));
+        $items->set('winner_ids',$ids);
 
-            return $items;
-        }
+        return $items;
+    }
 
 }
