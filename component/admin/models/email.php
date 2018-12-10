a3b71ac2a2911e20419ebb7ffa3bf1a642d29fac
RedfinDiver <redfindiver@gmail.com>
2018-12-10 20:57:15 +0100

    finished email feature in component

--- component/admin/models/email.php
+++ component/admin/models/email.php
@@ -10,6 +10,8 @@ defined('_JEXEC') or die;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Filesystem\File;
+
 
 /**
  * Model for email to members and newsletter subscribers
@@ -29,7 +31,8 @@ class TkdclubModelEmail extends JModelAdmin
 	public function getForm($data = array(), $loadData = true)
 	{
 		// Get the form.
-		$form = $this->loadForm('com_tkdclub.email', 'email', array('control' => 'jform', 'load_data' => $loadData));
+		$options = array('control' => 'jform', 'load_data' => $loadData);
+        $form = $this->loadForm('tkdclub', 'email',  $options);
 
 		if (empty($form))
 		{
@@ -49,7 +52,7 @@ class TkdclubModelEmail extends JModelAdmin
 	protected function loadFormData()
 	{
 		// Check the session for previously entered form data.
-		$data = Factory::getApplication()->getUserState('com_tkdclub.display.email.data', array());
+		$data = Factory::getApplication()->getUserState('com_tkdclub.email.data', array());
 
 		return $data;
 	}
@@ -63,42 +66,30 @@ class TkdclubModelEmail extends JModelAdmin
 	{
 		$app    = Factory::getApplication();
 		$user   = Factory::getUser();
-		$access = new JAccess;
         $params = ComponentHelper::getParams('com_tkdclub');
-		
-		// Get the data from the input
 		$data   = $app->input->post->get('jform', array(), 'array');
+
 		$subject      = array_key_exists('subject', $data) ? $data['subject'] : '';
 		$message_body = array_key_exists('message', $data) ? $data['message'] : '';
+		$active       = array_key_exists('active', $data) ? (int) $data['active'] : 0;
+		$inactive     = array_key_exists('inactive', $data) ? (int) $data['inactive'] : 0;
+		$supporter    = array_key_exists('supporter', $data) ? (int) $data['supporter'] : 0;
 
-		$recipients = $this->getRecipients();
-
-		return;
-
-		// Automatically removes html <formatting></formatting>
-        $message_body = JFilterInput::getInstance()->clean($message_body, 'string');
+		$recipients   = $this->getRecipients();
+		$attachments  = $this->getAttachments();
+		
 
 		// Check for a message body and subject
 		if (!$message_body || !$subject)
 		{
-			$app->setUserState('com_tkdclub.display.email.data', $data);
+			$app->setUserState('com_tkdclub.email.data', $data);
 			$this->setError(Text::_('COM_TKDCLUB_MAIL_PLEASE_FILL_IN_THE_FORM_CORRECTLY'));
 
 			return false;
 		}
-                
-		if (!$params->get('email_test')) // If test-email ist empty, proceed
-		{
-			
-		}				
-		elseif ($params->get('email_test')) //when test email ist set, just set it as only one
-		{
-			$rows = array($params->get('email_test'));
-		}
 
 		// Get the Mailer
 		$mailer = Factory::getMailer();
-		$params = ComponentHelper::getParams('com_tkdclub');
 
 		// Build email message
 		$mailer->setSender(array($user->get('email'), $user->get('name')));
@@ -106,8 +97,10 @@ class TkdclubModelEmail extends JModelAdmin
 		$mailer->setBody($message_body . $params->get('mail_signature'));
 
 		// Add recipients
-		$mailer->addBcc($rows);
-		$mailer->addRecipient($user->get('email')); //copy to sender
+		$mailer->addBcc($recipients);
+
+		// Add attachments
+		$mailer->addAttachment($attachments['paths'], $attachments['filenames']);
 
 		// Send the Mail
 		$rs = $mailer->Send();
@@ -115,14 +108,14 @@ class TkdclubModelEmail extends JModelAdmin
 		// Check for an error
 		if ($rs instanceof Exception)
 		{
-			$app->setUserState('com_tkdclub.display.email.data', $data);
+			$app->setUserState('com_tkdclub.email.data', $data);
 			$this->setError($rs->getError());
 
 			return false;
 		}
 		elseif (empty($rs))
 		{
-			$app->setUserState('com_tkdclub.display.email.data', $data);
+			$app->setUserState('com_tkdclub.email.data', $data);
 			$this->setError(Text::_('COM_TKDCLUB_MAIL_THE_MAIL_COULD_NOT_BE_SENT'));
 
 			return false;
@@ -130,20 +123,20 @@ class TkdclubModelEmail extends JModelAdmin
 		else
 		{
 			/**
-			 * Fill the data (specially for the 'mode', 'group' and 'bcc': they could not exist in the array
-			 * when the box is not checked and in this case, the default value would be used instead of the '0'
-			 * one)
+			 * Fill the data (specially for the 'active', 'inactive', 'supporter' and (in future versions) 'newsletter':
+			 * they could not exist in the array when the box is not checked and in this case, the default value would be
+			 * used instead of the '0' one
 			 */
 			$data['supporter'] = $supporter;
-                        $data['active']    = $active;
-                        $data['inactive']  = $inactive;
-  			$data['bcc']       = $bcc;    
-                        //prepared for future versions
-                        //$data['newsletter'] = $newsletter;
+            $data['active']    = $active;
+            $data['inactive']  = $inactive;  
  			$data['subject']   = $subject;
 			$data['message']   = $message_body;
-			$app->setUserState('com_tkdclub.display.email.data', array());
-			$app->enqueueMessage(Text::plural('COM_TKDCLUB_MAIL_EMAIL_SENT_TO_N_USERS', count($rows)), 'message');
+			// TODO prepared for future versions
+            // $data['newsletter'] = $newsletter;
+
+			$app->setUserState('com_tkdclub.email.data', array());
+			$app->enqueueMessage(Text::plural('COM_TKDCLUB_MAIL_EMAIL_SENT_TO_N_USERS', count($recipients)), 'message');
 
 			return true;
 		}
@@ -152,19 +145,19 @@ class TkdclubModelEmail extends JModelAdmin
 	/**
 	 * Get all unique email addresses from database 
 	 * 
-	 * @return	array	array with unique addresses
-	 * 
+	 * @return	mixed	array with unique addresses from database
+	 * 					array with test adress if given in configuration
 	 */
 	public function getRecipients()
 	{	
 		// First of all check for test address in configuration
-		if ($test_address = ComponentHelper::getParams('com_tkdclub')->get('email_test'))
+		if ($test_address[] = ComponentHelper::getParams('com_tkdclub')->get('email_test'))
 		{
 			return $test_address;
 		}
 
 		$app = Factory::getApplication();
-		$input  = $app->input->post->get('jform', array(), 'array');
+		$input = $app->input->post->get('jform', array(), 'array');
 		
 		// Create an array with recipients from the members table
 		$member_recipients = array();
@@ -172,14 +165,14 @@ class TkdclubModelEmail extends JModelAdmin
 		array_key_exists('inactive', $input) ? $member_recipients[] = 'inactive' : null;
 		array_key_exists('supporter', $input) ? $member_recipients[] = 'support' : null;
 
-		// Message to newsletter subscribers
-		array_key_exists('newsletter', $input) ? $newsletter = 'newsletter' : null;
+		// TODO Message to newsletter subscribers
+		// array_key_exists('newsletter', $input) ? $newsletter = 'newsletter' : null;
 
 		// Check for existing recipients
 		if (empty($member_recipients) && !$newsletter)
 		{
 			$app->enqueueMessage(Text::_('COM_TKDCLUB_NO_RECIPIENTS_SELECTED'), 'error');
-			$app->setUserState('com_tkdclub.display.email.data', $input);
+			$app->setUserState('com_tkdclub.email.data', $input);
 			
 			return false;
 		}
@@ -203,15 +196,16 @@ class TkdclubModelEmail extends JModelAdmin
 			$q_members->where('NOT ' . $db->quoteName('email') . ' = ' . $db->quote(''));
 		}
 
-		// Build query for newsletter subscribers table
+		/* TODO Build query for newsletter subscribers table
 		if ($newsletter)
 		{
 			$q_subscribers = $db->getQuery(true);
 			$q_subscribers->select('DISTINCT' . $db->quoteName('email'))->from($db->quoteName('#__tkdclub_newsletter_subscribers'));
 			$q_subscribers->where('NOT ' . $db->quoteName('email') . ' = ' . $db->quote(''));
-		}		
+		}
+		*/		
 		
-		// Now determine what query has to run
+		/* TODO Now determine what query has to run
 		if (!empty($member_recipients) && $newsletter)
 		{
 			$query = $q_members->union($q_subscribers);
@@ -226,7 +220,60 @@ class TkdclubModelEmail extends JModelAdmin
 		{
 			$query = $q_members;
 		}
+		*/
+
+		$query = $q_members;
 
 		return $db->setQuery($query)->loadColumn();
-	} 
+	}
+
+	/**
+	 * Get the attached files from the request
+	 * 
+	 * @return	array	array of file paths and names
+	 */
+	public function getAttachments()
+	{
+		$app   = Factory::getApplication();
+		$data  = $app->input->post->get('jform', array(), 'array');
+		$files = $app->input->files->get('jform');
+		$allowed_extensions = ComponentHelper::getParams('com_tkdclub')->get('allowed_extensions', 'pdf');
+		$allowed_extensions_array =  explode(',', $allowed_extensions);
+
+		$attachments = array();
+		$paths = array();
+		$filenames = array();
+
+		foreach($files as $file)
+		{
+			if($file['name'] != '' && $file['error'] == (int) 0)
+			{	
+				// Check for right file extension
+				if(!in_array(File::getExt($file['name']), $allowed_extensions_array))
+				{
+					$app->enqueueMessage(Text::_('COM_TKDCLUB_EMAIL_ATTACHMENT_NOT_ALLOWED') . $allowed_extensions, 'error');
+					$app->setUserState('com_tkdclub.email.data', $data);
+					
+					return false;	
+				}
+
+				$paths[] = $file['tmp_name'];
+				$filenames[] = $file['name'];
+
+			}
+
+		}
+
+		if(empty($paths))
+		{
+			return false;
+		}
+
+		$attachments['paths'] = $paths;
+		$attachments['filenames'] = $filenames;
+
+		return $attachments;
+
+	}
+
 }
