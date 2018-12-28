92d39bdac59e9ccf556b919d8ddb1fa63eb94980
RedfinDiver <redfindiver@gmail.com>
2018-12-28 01:30:24 +0100

    added forgotten drop table statement in uninstall

--- component/admin/sql/uninstall.mysql.sql
+++ component/admin/sql/uninstall.mysql.sql
@@ -4,4 +4,5 @@ DROP TABLE IF EXISTS `#__tkdclub_medals`;
 DROP TABLE IF EXISTS `#__tkdclub_promotions`;
 DROP TABLE IF EXISTS `#__tkdclub_candidates`;
 DROP TABLE IF EXISTS `#__tkdclub_events`;
+DROP TABLE IF EXISTS `#__tkdclub_event_participants`;
 DROP TABLE IF EXISTS `#__tkdclub_newsletter_subscribers`;
