0a39644b5dd22d1d39bf9bc0fc1b457b864b0bc8
RedfinDiver <redfindiver@gmail.com>
2019-08-31 00:56:26 +0200

    promotion and promotions view to namespaces

--- component/admin/views/promotion/tmpl/edit.php
+++ component/admin/views/promotion/tmpl/edit.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package    Taekwondo Club
  * @copyright  Copyright (C) 2018 Markus Moser. All rights reserved.
@@ -7,12 +8,17 @@
 
 defined('_JEXEC') or die;
 
-JHtml::_('behavior.formvalidation');
-JHtml::_('formbehavior.chosen', 'select');
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Router\Route;
 
-JHtml::stylesheet('administrator/components/com_tkdclub/assets/css/tkdclub.css');
+HTMLHelper::_('behavior.formvalidation');
+HTMLHelper::_('formbehavior.chosen', 'select');
 
-JFactory::getDocument()->addScriptDeclaration("
+HTMLHelper::stylesheet('administrator/components/com_tkdclub/assets/css/tkdclub.css');
+
+Factory::getDocument()->addScriptDeclaration("
 Joomla.submitbutton = function(task)
 {
     if (task == 'promotion.cancel' || document.formvalidator.isValid(document.getElementById('promotion-form'))) 
@@ -24,60 +30,55 @@ Joomla.submitbutton = function(task)
 
 ?>
 
-<form action="<?php echo JRoute::_('index.php?option=com_tkdclub&promotion_id=' .(int) $this->item->promotion_id); ?>"
-      method="post"
-      name="adminForm"
-      id="promotion-form"
-      class="form-validate">
+<form action="<?php echo Route::_('index.php?option=com_tkdclub&promotion_id=' . (int) $this->item->promotion_id); ?>" method="post" name="adminForm" id="promotion-form" class="form-validate">
 
     <div class="row-fluid">
         <div class="span12 form-horizontal">
             <fieldset>
-                <?php echo JHtml::_('bootstrap.startTabSet', 'myTab', array('active' => 'promotion')); ?>
-                    <?php echo JHtml::_('bootstrap.addTab', 'myTab', 'promotion', empty($this->item->promotion_id) ? JText::_('COM_TKDCLUB_PROMOTION_NEW_TAB', true) : JText::sprintf('COM_TKDCLUB_PROMOTION_EDIT_TAB', $this->item->promotion_id, true)); ?>
-                    <?php
-                        foreach($this->form->getFieldset('promotion_data') as $field):
-                            // If the field is hidden, render nothing
-                            if ($field->hidden):
+                <?php echo HTMLHelper::_('bootstrap.startTabSet', 'myTab', array('active' => 'promotion')); ?>
+                <?php echo HTMLHelper::_('bootstrap.addTab', 'myTab', 'promotion', empty($this->item->promotion_id) ? Text::_('COM_TKDCLUB_PROMOTION_NEW_TAB', true) : Text::sprintf('COM_TKDCLUB_PROMOTION_EDIT_TAB', $this->item->promotion_id, true)); ?>
+                <?php
+                foreach ($this->form->getFieldset('promotion_data') as $field) :
+                    // If the field is hidden, render nothing
+                    if ($field->hidden) :
 
-                            else:
-                            ?>
+                    else :
+                        ?>
 
-                            <div class="control-group">
-                                <div class="control-label"><?php echo $field->label; ?></div>
-                                <div class="controls"><?php echo $field->input ?></div>
-                            </div>
+                        <div class="control-group">
+                            <div class="control-label"><?php echo $field->label; ?></div>
+                            <div class="controls"><?php echo $field->input ?></div>
+                        </div>
 
-                            <?php
-                            endif;
-                        endforeach;
-                    ?>
+                <?php
+                    endif;
+                endforeach;
+                ?>
 
-                    <?php echo JHtml::_('bootstrap.endTab'); ?>
+                <?php echo HTMLHelper::_('bootstrap.endTab'); ?>
 
-                    <?php echo JHtml::_('bootstrap.addTab', 'myTab', 'item_data', JText::_('COM_TKDCLUB_ITEM_DATA', true)); ?>
-                        <?php if (empty($this->item->promotion_id)) : ?>
-                            <div class="alert alert-no-items">
-                                <?php echo JText::_('COM_TKDCLUB_NO_ITEM_DATA'); ?>
-                            </div>
-                        <?php else : ?>                          
-                            <div> 
-                                <?php foreach ($this->form->getFieldset('item_data') as $field) : ?>
-                                        <?php echo $field->renderField(); ?>
-                                <?php endforeach; ?>
-                            </div>
-                        <?php endif; ?>          
-                    <?php echo JHtml::_('bootstrap.endTab'); ?>
+                <?php echo HTMLHelper::_('bootstrap.addTab', 'myTab', 'item_data', Text::_('COM_TKDCLUB_ITEM_DATA', true)); ?>
+                <?php if (empty($this->item->promotion_id)) : ?>
+                    <div class="alert alert-no-items">
+                        <?php echo Text::_('COM_TKDCLUB_NO_ITEM_DATA'); ?>
+                    </div>
+                <?php else : ?>
+                    <div>
+                        <?php foreach ($this->form->getFieldset('item_data') as $field) : ?>
+                            <?php echo $field->renderField(); ?>
+                        <?php endforeach; ?>
+                    </div>
+                <?php endif; ?>
+                <?php echo HTMLHelper::_('bootstrap.endTab'); ?>
 
-                <?php echo JHtml::_('bootstrap.endTabSet'); ?>
+                <?php echo HTMLHelper::_('bootstrap.endTabSet'); ?>
             </fieldset>
-        </div>       
-    </div>   
+        </div>
+    </div>
 
     <div>
         <input type="hidden" name="task" value="" />
-        <?php echo JHtml::_('form.token'); ?>  
+        <?php echo HTMLHelper::_('form.token'); ?>
     </div>
 
 </form>
-
--- component/admin/views/promotions/view.html.php
+++ component/admin/views/promotions/view.html.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package    Taekwondo Club
  * @copyright  Copyright (C) 2018 Markus Moser. All rights reserved.
@@ -7,7 +8,12 @@
 
 defined('_JEXEC') or die;
 
-JLoader::register('TkdclubHelperActions', JPATH_COMPONENT_ADMINISTRATOR. '/helpers/actions.php');
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Toolbar\Toolbar;
+use Joomla\CMS\Toolbar\ToolbarHelper;
+use Joomla\CMS\Helper\ContentHelper;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
 
 /**
  * view-class for view: 'promotions'
@@ -29,79 +35,74 @@ class TkdClubViewPromotions extends JViewLegacy
         $this->allrows = $this->get('Allrows');
         $this->filterForm    = $this->get('FilterForm');
         $this->activeFilters = $this->get('ActiveFilters');
-        
+
         /* TODO mini statistics for promotions
-        $this->togglestats = JFactory::getSession()->get('togglestats_promotions', null, 'tkdclub');
+        $this->togglestats = Factory::getSession()->get('togglestats_promotions', null, 'tkdclub');
         
         if ($this->togglestats)
         {
             $this->promotiondata = $this->get('Promotionsdata');
         } */
-        
+
         $this->addToolbar();
         $this->sidebar = JHtmlSidebar::render();
         parent::display($tpl);
     }
-    
+
     protected function addToolbar()
     {
-        $clubname = JComponentHelper::getParams('com_tkdclub')->get('club_name', JText::_('COM_TKDCLUB'));
-        
-        JToolBarHelper::title($clubname . JText::_('COM_TKDCLUB_PROMOTION_ADMIN_VIEW'), 'tkdclub');
-        
-        $canDo = TkdClubHelperActions::getActions();
-        
-        JToolBarHelper::divider();
-        
-        if ($canDo->get('core.create'))
-        {
-            JToolBarHelper::addNew('promotion.add', 'JTOOLBAR_NEW');
+        $clubname = ComponentHelper::getParams('com_tkdclub')->get('club_name', Text::_('COM_TKDCLUB'));
+
+        ToolBarHelper::title($clubname . Text::_('COM_TKDCLUB_PROMOTION_ADMIN_VIEW'), 'tkdclub');
+
+        $canDo = ContentHelper::getActions('com_tkdclub');
+
+        ToolBarHelper::divider();
+
+        if ($canDo->get('core.create')) {
+            ToolBarHelper::addNew('promotion.add', 'JTOOLBAR_NEW');
         }
 
-        if ($canDo->get('core.edit'))
-        {
-            JToolBarHelper::editList('promotion.edit', 'JTOOLBAR_EDIT');   
+        if ($canDo->get('core.edit')) {
+            ToolBarHelper::editList('promotion.edit', 'JTOOLBAR_EDIT');
         }
 
-        if ($canDo->get('core.edit.state'))
-        {
-            JToolBarHelper::publish('promotions.publish', 'JTOOLBAR_CHECKIN', true);
-            JToolBarHelper::unpublish('promotions.unpublish', 'COM_TKDCLUB_PROMOTION_UNPUBLISH', true);
+        if ($canDo->get('core.edit.state')) {
+            ToolBarHelper::publish('promotions.publish', 'JTOOLBAR_CHECKIN', true);
+            ToolBarHelper::unpublish('promotions.unpublish', 'COM_TKDCLUB_PROMOTION_UNPUBLISH', true);
         }
 
-        if ($canDo->get('core.delete'))
-        {
-            JToolBarHelper::deleteList('COM_TKDCLUB_PROMOTION_DELETE_QUESTION', 'promotions.delete', 'JTOOLBAR_DELETE', true);
-        }
-        
-        if ($canDo->get('core.admin'))
-        {
-            JToolBarHelper::preferences('com_tkdclub');
+        if ($canDo->get('core.delete')) {
+            ToolBarHelper::deleteList('COM_TKDCLUB_PROMOTION_DELETE_QUESTION', 'promotions.delete', 'JTOOLBAR_DELETE', true);
         }
 
-        $toolbar = JToolbar::getInstance('toolbar');
-        $toolbar->addButtonPath(JPATH_COMPONENT.'/buttons');
+        if ($canDo->get('core.admin')) {
+            ToolBarHelper::preferences('com_tkdclub');
+        }
+
+        $toolbar = Toolbar::getInstance('toolbar');
+        $toolbar->addButtonPath(JPATH_COMPONENT . '/buttons');
 
         // TODO mini statistiks for promotions
         /* if ($this->togglestats)
         {
-            JToolBarHelper::custom('promotions.togglestats', 'eye-close', 'eye-close', 'COM_TKDCLUB_BUTTON_STATS', false);
+            ToolBarHelper::custom('promotions.togglestats', 'eye-close', 'eye-close', 'COM_TKDCLUB_BUTTON_STATS', false);
         }
         else
         {
-            JToolBarHelper::custom('promotions.togglestats', 'eye-open', 'eye-open', 'COM_TKDCLUB_BUTTON_STATS', false);
+            ToolBarHelper::custom('promotions.togglestats', 'eye-open', 'eye-open', 'COM_TKDCLUB_BUTTON_STATS', false);
         } */
 
         $toolbar->appendButton('RawFormat',  'download', 'COM_TKDCLUB_BUTTON_EXPORT', 'export.promotions');
-        
+
         $help_url  = 'https://tkdclub.readthedocs.io/{langcode}/latest/pruefungen.html';
-        JToolbarHelper::help( '', false, $help_url );
+        JToolbarHelper::help('', false, $help_url);
     }
 
     protected function getSortFields()
-	{
-		return array(
-			'date' => JText::_('COM_TKDCLUB_PROMOTION_DATE'),
-		);
-	}
+    {
+        return array(
+            'date' => Text::_('COM_TKDCLUB_PROMOTION_DATE'),
+        );
+    }
 }
