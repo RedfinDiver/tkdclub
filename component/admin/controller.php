f7395f008bce53ce3a56e927289f69842e5dcd47
RedfinDiver <redfindiver@gmail.com>
2019-08-31 14:15:38 +0200

    helper methods in one file now

--- component/admin/controller.php
+++ component/admin/controller.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package    Taekwondo Club
  * @copyright  Copyright (C) 2018 Markus Moser. All rights reserved.
@@ -6,25 +7,24 @@
  */
 
 defined('_JEXEC') or die;
-JLoader::register('TkdclubHelperSidebar', JPATH_COMPONENT_ADMINISTRATOR. '/helpers/sidebar.php');
+JLoader::register('Helper', JPATH_COMPONENT_ADMINISTRATOR . '/helpers/tkdclub.php');
 
 /**
  * TKD Club Main Controller
  */
 class TkdClubController extends JControllerLegacy
-{  
-    protected $default_view = 'members';
+{
+	protected $default_view = 'members';
 
-    public function display($cachable = false, $urlparams = false)
-	{    
+	public function display($cachable = false, $urlparams = false)
+	{
 		$view   = $this->input->get('view', $this->default_view);
 		$layout = $this->input->get('layout', 'default');
-		$id     = $this->input->getInt($view . '_id');     
-		TkdclubHelperSidebar::addSubmenu($view);
-		
+		$id     = $this->input->getInt($view . '_id');
+		Helper::addSubmenu($view);
+
 		// Check for edit form.
-		if ($view == 'member' && $layout == 'edit' && !$this->checkEditId('com_tkdclub.edit.member', $id))
-		{
+		if ($view == 'member' && $layout == 'edit' && !$this->checkEditId('com_tkdclub.edit.member', $id)) {
 			// Somehow the person just went to the form - we don't allow that.
 			$this->setError(JText::sprintf('JLIB_APPLICATION_ERROR_UNHELD_ID', $id));
 			$this->setMessage($this->getError(), 'error');
@@ -32,9 +32,8 @@ class TkdClubController extends JControllerLegacy
 
 			return false;
 		}
-		
-		if ($view == 'training' && $layout == 'edit' && !$this->checkEditId('com_tkdclub.edit.training', $id))
-		{
+
+		if ($view == 'training' && $layout == 'edit' && !$this->checkEditId('com_tkdclub.edit.training', $id)) {
 			// Somehow the person just went to the form - we don't allow that.
 			$this->setError(JText::sprintf('JLIB_APPLICATION_ERROR_UNHELD_ID', $id));
 			$this->setMessage($this->getError(), 'error');
@@ -43,8 +42,7 @@ class TkdClubController extends JControllerLegacy
 			return false;
 		}
 
-		if ($view == 'medal' && $layout == 'edit' && !$this->checkEditId('com_tkdclub.edit.medal', $id))
-		{
+		if ($view == 'medal' && $layout == 'edit' && !$this->checkEditId('com_tkdclub.edit.medal', $id)) {
 			// Somehow the person just went to the form - we don't allow that.
 			$this->setError(JText::sprintf('JLIB_APPLICATION_ERROR_UNHELD_ID', $id));
 			$this->setMessage($this->getError(), 'error');
@@ -53,8 +51,7 @@ class TkdClubController extends JControllerLegacy
 			return false;
 		}
 
-		if ($view == 'promotion' && $layout == 'edit' && !$this->checkEditId('com_tkdclub.edit.promotion', $id))
-		{
+		if ($view == 'promotion' && $layout == 'edit' && !$this->checkEditId('com_tkdclub.edit.promotion', $id)) {
 			// Somehow the person just went to the form - we don't allow that.
 			$this->setError(JText::sprintf('JLIB_APPLICATION_ERROR_UNHELD_ID', $id));
 			$this->setMessage($this->getError(), 'error');
@@ -63,8 +60,7 @@ class TkdClubController extends JControllerLegacy
 			return false;
 		}
 
-		if ($view == 'candidate' && $layout == 'edit' && !$this->checkEditId('com_tkdclub.edit.candidate', $id))
-		{
+		if ($view == 'candidate' && $layout == 'edit' && !$this->checkEditId('com_tkdclub.edit.candidate', $id)) {
 			// Somehow the person just went to the form - we don't allow that.
 			$this->setError(JText::sprintf('JLIB_APPLICATION_ERROR_UNHELD_ID', $id));
 			$this->setMessage($this->getError(), 'error');
@@ -73,8 +69,7 @@ class TkdClubController extends JControllerLegacy
 			return false;
 		}
 
-		if ($view == 'event' && $layout == 'edit' && !$this->checkEditId('com_tkdclub.edit.event', $id))
-		{
+		if ($view == 'event' && $layout == 'edit' && !$this->checkEditId('com_tkdclub.edit.event', $id)) {
 			// Somehow the person just went to the form - we don't allow that.
 			$this->setError(JText::sprintf('JLIB_APPLICATION_ERROR_UNHELD_ID', $id));
 			$this->setMessage($this->getError(), 'error');
@@ -83,8 +78,7 @@ class TkdClubController extends JControllerLegacy
 			return false;
 		}
 
-		if ($view == 'participant' && $layout == 'edit' && !$this->checkEditId('com_tkdclub.edit.participant', $id))
-		{
+		if ($view == 'participant' && $layout == 'edit' && !$this->checkEditId('com_tkdclub.edit.participant', $id)) {
 			// Somehow the person just went to the form - we don't allow that.
 			$this->setError(JText::sprintf('JLIB_APPLICATION_ERROR_UNHELD_ID', $id));
 			$this->setMessage($this->getError(), 'error');
@@ -93,6 +87,6 @@ class TkdClubController extends JControllerLegacy
 			return false;
 		}
 
-        parent::display($cachable, $urlparams);
+		parent::display($cachable, $urlparams);
 	}
 }
