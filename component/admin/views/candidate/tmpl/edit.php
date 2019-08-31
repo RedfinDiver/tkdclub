135b0404242cc94b847c1ea1e6661acc1a88b77b
RedfinDiver <redfindiver@gmail.com>
2019-08-31 14:26:58 +0200

    participant/-s view namespace

--- component/admin/views/candidate/tmpl/edit.php
+++ component/admin/views/candidate/tmpl/edit.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package    Taekwondo Club
  * @copyright  Copyright (C) 2018 Markus Moser. All rights reserved.
@@ -7,64 +8,62 @@
 
 defined('_JEXEC') or die;
 
-JHtml::_('behavior.formvalidation');
-JHtml::_('formbehavior.chosen', 'select');
-JHtml::_('script', 'administrator/components/com_tkdclub/assets/js/getcandidatedata.js');
-JHtml::stylesheet('administrator/components/com_tkdclub/assets/css/tkdclub.css');
-JLoader::register('Tkdclubmodelexamparts', JPATH_COMPONENT_ADMINISTRATOR .'/models/examparts.php');
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Router\Route;
+
+HTMLHelper::_('behavior.formvalidation');
+HTMLHelper::_('formbehavior.chosen', 'select');
+HTMLHelper::_('script', 'administrator/components/com_tkdclub/assets/js/getcandidatedata.js');
+HTMLHelper::stylesheet('administrator/components/com_tkdclub/assets/css/tkdclub.css');
+JLoader::register('Tkdclubmodelexamparts', JPATH_COMPONENT_ADMINISTRATOR . '/models/examparts.php');
 ?>
 
 <script type="text/javascript">
-	Joomla.submitbutton = function(task)
-	{
-		if (task == 'candidate.cancel' || document.formvalidator.isValid(document.id('candidate-form')))
-		{
-			Joomla.submitform(task, document.getElementById('candidate-form'));
-		}
-	}
+    Joomla.submitbutton = function(task) {
+        if (task == 'candidate.cancel' || document.formvalidator.isValid(document.id('candidate-form'))) {
+            Joomla.submitform(task, document.getElementById('candidate-form'));
+        }
+    }
 </script>
 
-<form action="<?php echo JRoute::_('index.php?option=com_tkdclub&id=' .(int) $this->item->id); ?>"
-      method="post"
-      name="adminForm"
-      id="candidate-form"
-      class="form-validate">
-    
-    <div class="row-fluid"> 
-		<div class="span12 form-horizontal">
-        <fieldset>
-            <?php echo JHtml::_('bootstrap.startTabSet', 'myTab', array('active' => 'exampart_data')); ?>
-                <?php echo JHtml::_('bootstrap.addTab', 'myTab', 'candidate_data', empty($this->item->id) ? JText::_('COM_TKDCLUB_CANDIDATE_ADD_TAB', true) : JText::sprintf('COM_TKDCLUB_CANDIDATE_EDIT_TAB', $this->item->id, true)); ?>
-                    <?php echo $this->form->renderField('test_state'); ?>
-                    <?php echo $this->form->renderField('id_promotion'); ?>
-                    <?php echo $this->form->renderField('id_candidate'); ?>
-                    <?php echo $this->form->renderField('lastpromotion'); ?>
-                    <?php echo $this->form->renderField('grade'); ?>
-                    <?php echo $this->form->renderField('grade_achieve'); ?>
-                    <?php echo $this->form->renderField('notes'); ?>
-                
-            <?php echo JHtml::_('bootstrap.endTab'); ?>
+<form action="<?php echo Route::_('index.php?option=com_tkdclub&id=' . (int) $this->item->id); ?>" method="post" name="adminForm" id="candidate-form" class="form-validate">
 
-            <?php echo JHtml::_('bootstrap.addTab', 'myTab', 'item_data', JText::_('COM_TKDCLUB_ITEM_DATA', true)); ?>
-                    <?php if (empty($this->item->id)) : ?>
-                        <div class="alert alert-no-items">
-                            <?php echo JText::_('COM_TKDCLUB_NO_ITEM_DATA'); ?>
-                        </div>
-                    <?php else : ?>                          
-                        <div> 
-                            <?php foreach ($this->form->getFieldset('item_data') as $field) : ?>
-                                <?php echo $field->renderField(); ?>
-                            <?php endforeach; ?>
-                        </div>
-                    <?php endif; ?>          
-                <?php echo JHtml::_('bootstrap.endTab'); ?>                 
-            <?php echo JHtml::_('bootstrap.endTabSet'); ?>
-        </fieldset>
-    </div>  
-    
-    <input type="hidden" name="task" value="" />
-    <?php   echo JHtml::_('form.token'); ?>
+    <div class="row-fluid">
+        <div class="span12 form-horizontal">
+            <fieldset>
+                <?php echo HTMLHelper::_('bootstrap.startTabSet', 'myTab', array('active' => 'exampart_data')); ?>
+                <?php echo HTMLHelper::_('bootstrap.addTab', 'myTab', 'candidate_data', empty($this->item->id) ? Text::_('COM_TKDCLUB_CANDIDATE_ADD_TAB', true) : Text::sprintf('COM_TKDCLUB_CANDIDATE_EDIT_TAB', $this->item->id, true)); ?>
+                <?php echo $this->form->renderField('test_state'); ?>
+                <?php echo $this->form->renderField('id_promotion'); ?>
+                <?php echo $this->form->renderField('id_candidate'); ?>
+                <?php echo $this->form->renderField('lastpromotion'); ?>
+                <?php echo $this->form->renderField('grade'); ?>
+                <?php echo $this->form->renderField('grade_achieve'); ?>
+                <?php echo $this->form->renderField('notes'); ?>
 
-    <div id="waittext" class="hidden"><?php echo JText::_('COM_TKDCLUB_WAIT_FOR_AJAX_RESPONSE'); ?></div>
-    <div id="errortext" class="hidden"><?php echo JText::_('COM_TKDCLUB_ERROR_CHECK_MESSAGES'); ?></div>
+                <?php echo HTMLHelper::_('bootstrap.endTab'); ?>
+
+                <?php echo HTMLHelper::_('bootstrap.addTab', 'myTab', 'item_data', Text::_('COM_TKDCLUB_ITEM_DATA', true)); ?>
+                <?php if (empty($this->item->id)) : ?>
+                    <div class="alert alert-no-items">
+                        <?php echo Text::_('COM_TKDCLUB_NO_ITEM_DATA'); ?>
+                    </div>
+                <?php else : ?>
+                    <div>
+                        <?php foreach ($this->form->getFieldset('item_data') as $field) : ?>
+                            <?php echo $field->renderField(); ?>
+                        <?php endforeach; ?>
+                    </div>
+                <?php endif; ?>
+                <?php echo HTMLHelper::_('bootstrap.endTab'); ?>
+                <?php echo HTMLHelper::_('bootstrap.endTabSet'); ?>
+            </fieldset>
+        </div>
+
+        <input type="hidden" name="task" value="" />
+        <?php echo HTMLHelper::_('form.token'); ?>
+
+        <div id="waittext" class="hidden"><?php echo Text::_('COM_TKDCLUB_WAIT_FOR_AJAX_RESPONSE'); ?></div>
+        <div id="errortext" class="hidden"><?php echo Text::_('COM_TKDCLUB_ERROR_CHECK_MESSAGES'); ?></div>
 </form>
--- component/admin/views/candidate/view.html.php
+++ component/admin/views/candidate/view.html.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package    Taekwondo Club
  * @copyright  Copyright (C) 2018 Markus Moser. All rights reserved.
@@ -7,7 +8,11 @@
 
 defined('_JEXEC') or die;
 
-JLoader::register('TkdclubHelperActions', JPATH_COMPONENT_ADMINISTRATOR. '/helpers/actions.php');
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Toolbar\ToolbarHelper;
+use Joomla\CMS\Helper\ContentHelper;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
 
 /**
  * View-class of edit-screen 'candidate'
@@ -16,46 +21,43 @@ class TkdClubViewCandidate extends JViewLegacy
 {
     protected $item;
     protected $form;
-    
+
     public function display($tpl = null)
     {
-        JFactory::getApplication()->input->set('hidemainmenu', true);
-        
+        Factory::getApplication()->input->set('hidemainmenu', true);
+
         $this->form = $this->get('Form');
         $this->item = $this->get('Item');
-        
+
         $this->addToolbar();
         parent::display($tpl);
     }
-    
+
     protected function addToolbar()
     {
-        $clubname = JComponentHelper::getParams('com_tkdclub')->get('club_name', JText::_('COM_TKDCLUB'));
-        
-        if ($this->item->id == NULL)
-        {
-            JToolBarHelper::title($clubname . JText::_('COM_TKDCLUB_CANDIDATE_NEW_TITLE'), 'tkdclub');
+        $clubname = ComponentHelper::getParams('com_tkdclub')->get('club_name', Text::_('COM_TKDCLUB'));
+
+        if ($this->item->id == NULL) {
+            ToolbarHelper::title($clubname . Text::_('COM_TKDCLUB_CANDIDATE_NEW_TITLE'), 'tkdclub');
+        } else {
+            ToolbarHelper::title($clubname . Text::_('COM_TKDCLUB_CANDIDATE_EDIT_TITLE'), 'tkdclub');
         }
-        else
-        {
-            JToolBarHelper::title($clubname . JText::_('COM_TKDCLUB_CANDIDATE_EDIT_TITLE'), 'tkdclub');
+
+        $canDo = ContentHelper::getActions('com_tkdclub');
+
+        ToolbarHelper::apply('candidate.apply', 'JTOOLBAR_APPLY');
+
+        ToolbarHelper::save('candidate.save', 'JTOOLBAR_SAVE');
+
+        if ($canDo->get('core.create')) {
+            ToolbarHelper::save2new('candidate.save2new');
         }
-      
-        $canDo = TkdClubHelperActions::getActions();
-        
-        JToolBarHelper::apply('candidate.apply', 'JTOOLBAR_APPLY');
 
-        JToolBarHelper::save('candidate.save', 'JTOOLBAR_SAVE');
+        ToolbarHelper::cancel('candidate.cancel', 'JTOOLBAR_CANCEL');
 
-        if ($canDo->get('core.create'))
-        {JToolBarHelper::save2new('candidate.save2new');}
-
-        JToolBarHelper::cancel('candidate.cancel', 'JTOOLBAR_CANCEL');
-        
-        JToolbarHelper::help('', '', 'http://tkdclub.readthedocs.io/de/latest/pruefungsteilnehmer.html');
+        ToolbarHelper::help('', '', 'http://tkdclub.readthedocs.io/de/latest/pruefungsteilnehmer.html');
 
         $help_url  = 'https://tkdclub.readthedocs.io/{langcode}/latest/pruefungsteilnehmer.html';
-        JToolbarHelper::help( '', false, $help_url );
+        ToolbarHelper::help('', false, $help_url);
     }
-    
 }
