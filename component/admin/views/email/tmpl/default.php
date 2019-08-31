ee11f2eb469dc02b1ea5ee9ad996b268e7acccf6
RedfinDiver <redfindiver@gmail.com>
2019-08-31 14:55:55 +0200

    email namespace

--- component/admin/views/email/tmpl/default.php
+++ component/admin/views/email/tmpl/default.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package    Taekwondo Club
  * @copyright  Copyright (C) 2018 Markus Moser. All rights reserved.
@@ -7,89 +8,85 @@
 
 defined('_JEXEC') or die;
 
-JHtml::_('behavior.core');
-JHtml::_('behavior.formvalidator');
-JHtml::_('formbehavior.chosen', 'select');
-JHtml::stylesheet('administrator/components/com_tkdclub/assets/css/tkdclub.css');
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Router\Route;
+
+HTMLHelper::_('behavior.core');
+HTMLHelper::_('behavior.formvalidator');
+HTMLHelper::_('formbehavior.chosen', 'select');
+HTMLHelper::stylesheet('administrator/components/com_tkdclub/assets/css/tkdclub.css');
 
 //test email active
-if ($this->email_test)
-{
-    JFactory::getApplication()->enqueueMessage(JText::_('COM_TKDCLUB_EMAIL_TESTMAIL_ACTIVE'). $this->email_test, 'message');
+if ($this->email_test) {
+    Factory::getApplication()->enqueueMessage(Text::_('COM_TKDCLUB_EMAIL_TESTMAIL_ACTIVE') . $this->email_test, 'message');
 }
 ?>
 
 <script type="text/javascript">
-    
     Joomla.submitbutton = function(pressbutton) {
         var form = document.adminForm;
-        
+
         if (pressbutton == 'email.cancel') {
-        Joomla.submitform(pressbutton);
-        return;
+            Joomla.submitform(pressbutton);
+            return;
         }
-        
+
         // do field validation
-        if (form.jform_subject.value == ""){
-            alert(Joomla.JText._('COM_TKDCLUB_EMAIL_PLEASE_FILL_IN_THE_SUBJECT'));
-        }
-        else if (form.jform_message.value == ""){
-            alert(Joomla.JText._('COM_TKDCLUB_EMAIL_PLEASE_FILL_IN_THE_MESSAGE'));
-        } 
-        else {
+        if (form.jform_subject.value == "") {
+            alert(Joomla.Text._('COM_TKDCLUB_EMAIL_PLEASE_FILL_IN_THE_SUBJECT'));
+        } else if (form.jform_message.value == "") {
+            alert(Joomla.Text._('COM_TKDCLUB_EMAIL_PLEASE_FILL_IN_THE_MESSAGE'));
+        } else {
             Joomla.submitform(pressbutton);
         }
     }
 </script>
 
-<?php if (!empty( $this->sidebar)) : ?>
-	<div id="j-sidebar-container" class="span2">
-		<?php echo $this->sidebar; ?>
-	</div>
-	<div id="j-main-container" class="span10">
-<?php else : ?>
-	<div id="j-main-container">
-<?php endif;?>
-
-<form action="<?php echo JRoute::_('index.php?option=com_tkdclub&view=email'); ?>"
-    name="adminForm"
-    method="post"
-    id="adminForm"
-    class="form-validate"
-    enctype="multipart/form-data">
-	
-        <div class="row-fluid">
-            <div class="span8">
-                <fieldset class="adminform">
-                <?php foreach ($this->form->getFieldset('email') as $field) : ?>
-                    <?php echo $field->renderField(); ?>
-                <?php endforeach; ?>
-                </fieldset>
-                <input type="hidden" name="task" value="" />
-                <?php echo JHtml::_('form.token'); ?>
-                
-            </div>
-            <?php if (!$this->email_test) : ?>
-                <div class="span4">
-                    <h4><?php echo JText::_('COM_TKDCLUB_EMAIL_RECIPIENTS')?></h4>
-                    <fieldset class="form-vertical">
-                        <div class="control-group checkbox">
-                            <div class="controls"><?php echo $this->form->getInput('active'); ?> <?php echo $this->form->getLabel('active'); ?></div>
-                        </div>
-                        <div class="control-group checkbox">
-                            <div class="control-label"><?php echo $this->form->getInput('supporter'); ?> <?php echo $this->form->getLabel('supporter'); ?></div>
-                        </div>
-                        <div class="control-group checkbox">
-                            <div class="control-label"><?php echo $this->form->getInput('inactive'); ?> <?php echo $this->form->getLabel('inactive'); ?></div>
-                        </div>
-                        <div class="control-group checkbox">
-                            <div class="control-label"><?php echo $this->form->getInput('newsletter'); ?> <?php echo $this->form->getLabel('newsletter'); ?></div>
-                        </div>
-                        <div class="control-label"><?php echo $this->form->getLabel('event'); ?> </div>
-                        <div class="controls"><?php echo $this->form->getInput('event'); ?> </div>
-                    </fieldset>
-                </div>
-            <?php endif; ?>
-        </div>
+<?php if (!empty($this->sidebar)) : ?>
+    <div id="j-sidebar-container" class="span2">
+        <?php echo $this->sidebar; ?>
     </div>
-</form>
+    <div id="j-main-container" class="span10">
+    <?php else : ?>
+        <div id="j-main-container">
+        <?php endif; ?>
+
+        <form action="<?php echo Route::_('index.php?option=com_tkdclub&view=email'); ?>" name="adminForm" method="post" id="adminForm" class="form-validate" enctype="multipart/form-data">
+
+            <div class="row-fluid">
+                <div class="span8">
+                    <fieldset class="adminform">
+                        <?php foreach ($this->form->getFieldset('email') as $field) : ?>
+                            <?php echo $field->renderField(); ?>
+                        <?php endforeach; ?>
+                    </fieldset>
+                    <input type="hidden" name="task" value="" />
+                    <?php echo HTMLHelper::_('form.token'); ?>
+
+                </div>
+                <?php if (!$this->email_test) : ?>
+                    <div class="span4">
+                        <h4><?php echo Text::_('COM_TKDCLUB_EMAIL_RECIPIENTS') ?></h4>
+                        <fieldset class="form-vertical">
+                            <div class="control-group checkbox">
+                                <div class="controls"><?php echo $this->form->getInput('active'); ?> <?php echo $this->form->getLabel('active'); ?></div>
+                            </div>
+                            <div class="control-group checkbox">
+                                <div class="control-label"><?php echo $this->form->getInput('supporter'); ?> <?php echo $this->form->getLabel('supporter'); ?></div>
+                            </div>
+                            <div class="control-group checkbox">
+                                <div class="control-label"><?php echo $this->form->getInput('inactive'); ?> <?php echo $this->form->getLabel('inactive'); ?></div>
+                            </div>
+                            <div class="control-group checkbox">
+                                <div class="control-label"><?php echo $this->form->getInput('newsletter'); ?> <?php echo $this->form->getLabel('newsletter'); ?></div>
+                            </div>
+                            <div class="control-label"><?php echo $this->form->getLabel('event'); ?> </div>
+                            <div class="controls"><?php echo $this->form->getInput('event'); ?> </div>
+                        </fieldset>
+                    </div>
+                <?php endif; ?>
+            </div>
+        </div>
+        </form>
--- component/admin/views/email/view.html.php
+++ component/admin/views/email/view.html.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package    Taekwondo Club
  * @copyright  Copyright (C) 2018 Markus Moser. All rights reserved.
@@ -7,49 +8,52 @@
 
 defined('_JEXEC') or die;
 
-JText::script('COM_TKDCLUB_EMAIL_PLEASE_FILL_IN_THE_SUBJECT');
-JText::script('COM_TKDCLUB_EMAIL_PLEASE_FILL_IN_THE_MESSAGE');
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Toolbar\ToolbarHelper;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+
+Text::script('COM_TKDCLUB_EMAIL_PLEASE_FILL_IN_THE_SUBJECT');
+Text::script('COM_TKDCLUB_EMAIL_PLEASE_FILL_IN_THE_MESSAGE');
 
 /**
  * View class for email view
  */
 class TkdClubViewEmail extends JViewLegacy
 {
-    
+
     protected $form;
-    
+
     protected $email_test;
-    
+
     public function display($tpl = null)
     {
         // Get data from the model
-		$this->form = $this->get('Form');
+        $this->form = $this->get('Form');
 
-		$this->addToolbar();
-                $this->getTestmail();
-                $this->sidebar = JHtmlSidebar::render();
-		parent::display($tpl);
-        
+        $this->addToolbar();
+        $this->getTestmail();
+        $this->sidebar = JHtmlSidebar::render();
+        parent::display($tpl);
     }
-    
+
     protected function addToolbar()
     {
-		JFactory::getApplication()->input->set('hidemainmenu', false);
-        
-        $clubname = JComponentHelper::getParams('com_tkdclub')->get('club_name', JText::_('COM_TKDCLUB'));
-        JToolBarHelper::title($clubname . JText::_('COM_TKDCLUB_EMAIL_ADMIN_VIEW'), 'tkdclub');
+        Factory::getApplication()->input->set('hidemainmenu', false);
 
-        JToolbarHelper::custom('email.send', 'envelope.png', 'send_f2.png', 'COM_TKDCLUB_EMAIL_TOOLBAR_SEND', false);
-		JToolbarHelper::divider();
-        JToolbarHelper::preferences('com_tkdclub');
-        
+        $clubname = ComponentHelper::getParams('com_tkdclub')->get('club_name', Text::_('COM_TKDCLUB'));
+        ToolBarHelper::title($clubname . Text::_('COM_TKDCLUB_EMAIL_ADMIN_VIEW'), 'tkdclub');
+
+        ToolBarHelper::custom('email.send', 'envelope.png', 'send_f2.png', 'COM_TKDCLUB_EMAIL_TOOLBAR_SEND', false);
+        ToolBarHelper::divider();
+        ToolBarHelper::preferences('com_tkdclub');
+
         $help_url  = 'https://tkdclub.readthedocs.io/{langcode}/latest/email.html';
-        JToolbarHelper::help( '', false, $help_url );
+        ToolBarHelper::help('', false, $help_url);
     }
-    
+
     protected function getTestmail()
     {
-        $this->email_test = JComponentHelper::getParams('com_tkdclub')->get('email_test');
+        $this->email_test = ComponentHelper::getParams('com_tkdclub')->get('email_test');
     }
-           
 }
